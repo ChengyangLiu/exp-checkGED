a359665bcde87dfff9abee4a34c8d8b9baa3d505
ChengyangLiu <liuchengyang1995@163.com>
2019-03-08 17:04:52 +0800

    fix bug

--- script/run.sh
+++ script/run.sh
@@ -4,21 +4,19 @@ ROOT=`git rev-parse --show-toplevel`
 
 GRAPH_FILE=../data/dbpedia_label39      # graph file path
 GED_FILE=../data/dbpedia_label39.ged    # ged file path
-GED_PART=64    # parts number
+GED_PART=16    # parts number
 
 cd build
 cmake ..
 make
 
-echo $GRAPH_FILE
-echo $GED_FILE
-echo $GED_PART
-
+echo "delivery into "$GED_PART" parts."
 ./exp-checkGED $GRAPH_FILE $GED_FILE $GED_PART
+echo "completed!"
 
 for ((i=0; i<$GED_PART; i++))
 do
   GED_PART_FILE=$GED_FILE$i
-  ./exp-checkGED $GRAPH_FILE $GED_PART_FILE
-  echo $GED_PART_FILE
+  GED_PART_FILE_LOG=$GED_FILE$i".log"
+  nohup ./exp-checkGED $GRAPH_FILE $GED_PART_FILE >$GED_PART_FILE_LOG 2>&1 &
 done
