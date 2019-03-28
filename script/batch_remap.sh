98b8491d04b664c0ef4305e8c529fea464ae5580
ChengyangLiu <liuchengyang1995@163.com>
2019-03-28 13:24:00 +0800

    add remap script

--- script/batch_remap.sh
+++ script/batch_remap.sh
@@ -15,26 +15,17 @@ GRAPH_FILE=$1          # graph file path
 ########################################
 # Modify parameters to fit your case.  #
 ########################################
-for ((i=0; i<=10; i++))
+for ((i=0; i<=54; i++))
 do
 
 GED_FILE=$2$i".gfd"    # gfd file path
 #GED_FILE=$2$i".ged"    # ged file path
 ########################################
 
-GRAPH_REMAP_FIEL=$GRAPH_FILE".remap"   # graph map
-GED_REMAP_FIEL=$GED_FILE".remap"       # ged map
-GED_LOG=$GED_REMAP_FIEL".log"          # geds' literals check log
-
 REMAP="./exp-checkGED 3 $GRAPH_FILE $GED_FILE"
-BOOST_VF2="nohup ./exp-checkGED 1 $GRAPH_REMAP_FIEL $GED_REMAP_FIEL >$GED_LOG 2>&1"
 
 echo "  Begin remap Graph file and GEDs file..."
 echo $REMAP
 eval $REMAP
-
-echo "  Running validation for GEDs..."
-echo $BOOST_VF2
-eval $BOOST_VF2
 
 done
--- script/batch_select.sh
+++ script/batch_select.sh
@@ -10,14 +10,16 @@ cd build
 cmake -DBOOST_GRAPH=TRUE ..
 make
 
-T_FILE=$1".vali_m"$2
+TYPE=".gfd"
+#TYPE=".ged"
+
+T_FILE=$1$TYPE".vali_m"$2
 T_FILE_TMP=$T_FILE".tmp"
 
-for ((i=0; i<=38; i++))
+for ((i=0; i<=54; i++))
 do
 
-GED_FILE=$1$i".gfd.remap"    # gfd file path
-#GED_FILE=$1$i".ged.remap"    # ged file path
+GED_FILE=$1$i$TYPE".remap"    # file path
 
 RES_FILE=$GED_FILE".vali_m"$2
 
